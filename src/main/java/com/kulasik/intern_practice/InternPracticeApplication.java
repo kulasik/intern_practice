62575bf716d2c261e5736c41134da2c1e1917e9c
Krzysztof <krzysztof@kulasik.pl>
2024-02-25 00:39:04 +0100

    init commit

---
+++ src/main/java/com/kulasik/intern_practice/InternPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kulasik.intern_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kulasik/intern_practice/InternPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulasik.intern_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
