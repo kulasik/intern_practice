d903207553b857b55d2c0b5a731d0158572bb788
Krzysztof <krzysztof@kulasik.pl>
2024-02-25 19:50:05 +0100

    add initDB

--- src/main/java/com/kulasik/intern_practice/InternPracticeApplication.java
+++ src/main/java/com/kulasik/intern_practice/InternPracticeApplication.java
@@ -1,7 +1,13 @@
 package com.kulasik.intern_practice;
 
+import com.kulasik.intern_practice.student.Student;
+import com.kulasik.intern_practice.student.service.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class InternPracticeApplication {
@@ -10,4 +16,12 @@ public class InternPracticeApplication {
         SpringApplication.run(InternPracticeApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initDB(StudentService studentService) {
+        return args -> {
+            studentService.addStudent(new Student("Mariusz", "Kowalski", LocalDate.now().minusYears(10)));
+            studentService.addStudent(new Student("Szymon", "Malczewski", LocalDate.now().minusYears(13)));
+            studentService.addStudent(new Student("Mariusz", "Pudzianowski", LocalDate.now().minusYears(34)));
+        };
+    }
 }
